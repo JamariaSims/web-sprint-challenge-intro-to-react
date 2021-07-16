2eb88e52a06b8ed137a7d130321864d6aeb5ca8a
Jamaria Sims <75871993+UnloadedZero@users.noreply.github.com>
2021-07-16 16:29:17 -0400

    Update2

--- src/components/Character.js
+++ src/components/Character.js
@@ -2,50 +2,21 @@
 import React from "react";
 import styled from "styled-components";
 const Title = styled.h1``;
+const characterView = styled.div;
 const Character = (props) => {
 	const { character } = props;
 	const characterData = Object.keys(character);
 	return (
-		<div>
-			<Title>{character[characterData[0]]}</Title>
-			<div>
-				<ul>
-					<ol key={Math.floor(Math.random() * 1000)}>
-						{character[characterData[1]]}
-					</ol>
-					<ol key={Math.floor(Math.random() * 1000)}>
-						{character[characterData[2]]}
-					</ol>
-					<ol key={Math.floor(Math.random() * 1000)}>
-						{character[characterData[3]]}
-					</ol>
-					<ol key={Math.floor(Math.random() * 1000)}>
-						{character[characterData[4]]}
-					</ol>
-					<ol key={Math.floor(Math.random() * 1000)}>
-						{character[characterData[5]]}
-					</ol>
-					<ol key={Math.floor(Math.random() * 1000)}>
-						{character[characterData[6]]}
-					</ol>
-					<ol key={Math.floor(Math.random() * 1000)}>
-						{character[characterData[7]]}
-					</ol>
-					<ol key={Math.floor(Math.random() * 1000)}>
-						{character[characterData[8]]}
-					</ol>
-					<ol key={Math.floor(Math.random() * 1000)}>
-						{character[characterData[9]]}
-					</ol>
-					<ol key={Math.floor(Math.random() * 1000)}>
-						{character[characterData[10]]}
-					</ol>
-					<ol key={Math.floor(Math.random() * 1000)}>
-						{character[characterData[11]]}
-					</ol>
-				</ul>
-			</div>
-		</div>
+		<characterView>
+			<Title>{character["name"]}</Title>
+			{characterData.map((charData) => {
+				return charData === "name" ? null : (
+					<p
+						className={charData + character["name"]}
+					>{`${charData}: ${character[charData]}`}</p>
+				);
+			})}
+		</characterView>
 	);
 };
 
