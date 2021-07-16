339f5871ea3e330ee4e4c3cab19503045a2360b5
Jamaria Sims <75871993+UnloadedZero@users.noreply.github.com>
2021-07-16 16:12:00 -0400

    Update1

--- src/App.js
+++ src/App.js
@@ -1,19 +1,30 @@
-import React from 'react';
-import './App.css';
-
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import "./App.css";
+import Character from "./components/Character";
 const App = () => {
-  // Try to think through what state you'll need for this app before starting. Then build out
-  // the state properties here.
+	// Try to think through what state you'll need for this app before starting. Then build out
+	// the state properties here.
 
-  // Fetch characters from the API in an effect hook. Remember, anytime you have a 
-  // side effect in a component, you want to think about which state and/or props it should
-  // sync up with, if any.
+	// Fetch characters from the API in an effect hook. Remember, anytime you have a
+	// side effect in a component, you want to think about which state and/or props it should
+	// sync up with, if any.
 
-  return (
-    <div className="App">
-      <h1 className="Header">Characters</h1>
-    </div>
-  );
-}
+	const [character, setCharacter] = useState([]);
+	useEffect(() => {
+		axios.get("https://swapi.dev/api/people").then((response) => {
+			setCharacter(response.data);
+		});
+	}, []);
+
+	return (
+		<div className="App">
+			<h1 className="Header">Characters</h1>
+			{character.map((char) => (
+				<Character character={char} />
+			))}
+		</div>
+	);
+};
 
 export default App;
--- src/components/Character.js
+++ src/components/Character.js
@@ -1,1 +1,52 @@
 // Write your Character component here
+import React from "react";
+import styled from "styled-components";
+const Title = styled.h1``;
+const Character = (props) => {
+	const { character } = props;
+	const characterData = Object.keys(character);
+	return (
+		<div>
+			<Title>{character[characterData[0]]}</Title>
+			<div>
+				<ul>
+					<ol key={Math.floor(Math.random() * 1000)}>
+						{character[characterData[1]]}
+					</ol>
+					<ol key={Math.floor(Math.random() * 1000)}>
+						{character[characterData[2]]}
+					</ol>
+					<ol key={Math.floor(Math.random() * 1000)}>
+						{character[characterData[3]]}
+					</ol>
+					<ol key={Math.floor(Math.random() * 1000)}>
+						{character[characterData[4]]}
+					</ol>
+					<ol key={Math.floor(Math.random() * 1000)}>
+						{character[characterData[5]]}
+					</ol>
+					<ol key={Math.floor(Math.random() * 1000)}>
+						{character[characterData[6]]}
+					</ol>
+					<ol key={Math.floor(Math.random() * 1000)}>
+						{character[characterData[7]]}
+					</ol>
+					<ol key={Math.floor(Math.random() * 1000)}>
+						{character[characterData[8]]}
+					</ol>
+					<ol key={Math.floor(Math.random() * 1000)}>
+						{character[characterData[9]]}
+					</ol>
+					<ol key={Math.floor(Math.random() * 1000)}>
+						{character[characterData[10]]}
+					</ol>
+					<ol key={Math.floor(Math.random() * 1000)}>
+						{character[characterData[11]]}
+					</ol>
+				</ul>
+			</div>
+		</div>
+	);
+};
+
+export default Character;
